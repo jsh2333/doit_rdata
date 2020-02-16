d2fd4e9d39de2231c49f88d0996f7d11ff50bdd5
jsh2333 <jsh2333@gmail.com>
2020-02-16 14:52:03 +0900

    mariadb 설치/설정법

--- ch12/ch12_script_works.r
+++ ch12/ch12_script_works.r
@@ -2,5 +2,15 @@
 # ch12  인터랙티브 그래프
 #############################
 
+mariadb  설치
+다운로드 받은 mariadb.zip을 압축을 풀어 놓습니다. 
+c:\works\mariadb 폴더에다 풀어놓았다고 가정하겠습니다. 
+
+설정방법
+관리자 계정으로 cmd 창을 열어 놓습니다. 
+> cd c:\works\mariadb  이동합니다. 
+> mysql_install_db.exe  --datadir=c:\works\mariadb\data --service="mariadb"  --port=3306  --password=pass
+
+
 
 
